e5c038e0bafae7ed3a6a800b072bfd75aaef2dcd
Asdrubal Suarez <asdrubal.ivan.suarez.rivera@gmail.com>
2019-07-18 17:40:47 -0300

    Adding calls that we are going to use for the board creation process

--- src/controllers/minesweeper.js
+++ src/controllers/minesweeper.js
@@ -1,7 +1,10 @@
-const { generateBoard } = require('../services/minesweeper');
+const { generateBoard } = require("../services/minesweeper");
+const { processBoard } = require("../utils");
 
-const createBoard = (req, res) => {
-
+const createBoard = async (req, res) => {
+  const board = await generateBoard();
+  const processedBoard = processBoard();
+  return res.json(processedBoard);
 };
 
 module.exports = {
